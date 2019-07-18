4d9093a3a677f1f2429f19f7bbc22fc35227f868
Pawling <bpawling@eaest.com>
2019-07-18 14:58:32 -0400

    4

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-src="https://js.arcgis.com/4.12/"></script>
+require([
+          "esri/Map",
+          "esri/views/MapView",
+          "dojo/domReady!"
+      ],
+      function(
+          Map, MapView
+      ) {
+          //create map 
+          var map = new Map({
+              basemap: "topo"
+          });
 
-    <script>
-      require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "terrain"
-        });
+          var lon = -95.7129;
+          lat = 37.0902;
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [-95, 40] // longitude, latitude
-        });
+          //view the map 
+          var view = new MapView({
+              container: "viewDiv",
+              map: map,
+              zoom: 4,
+              center: [lon, lat]
+          });
       });
-     </script>
