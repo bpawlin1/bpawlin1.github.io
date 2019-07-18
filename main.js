24072ce9e7be0a6e655d90833f1d5a0e3a05105c
Pawling <bpawling@eaest.com>
2019-07-18 16:10:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 require([
           "esri/Map",
           "esri/views/MapView",
+	  "esri/geometry/Point",
+          "esri/symbols/SimpleMarkerSymbol",
+	  "esri/graphic",
           "dojo/domReady!"
       ],
       function(
-          Map, MapView
-      ) {
+          Map, MapView, Point) {
           //create map 
           var map = new Map({
               basemap: "topo"
@@ -13,6 +15,8 @@ require([
 
           var lon = -95.7129;
           lat = 37.0902;
+
+	  view.graphics.add(ptGraphic);
 
           //view the map 
           var view = new MapView({
@@ -22,3 +26,18 @@ require([
               center: [lon, lat]
           });
       });
+var pt = new Point({
+  latitude: 40.792,
+  longitude: -77.871 
+});
+
+var sym = new SimpleMarkerSymbol({
+  color: "blue",
+  style: "square",
+  size: 12
+});
+
+var ptGraphic = new Graphic({
+  geometry:pt,
+  symbol:sym
+});
