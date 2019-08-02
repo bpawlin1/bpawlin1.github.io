917b2ede213fb830d1b738d0a18f7d656d302446
Pawling <bpawling@eaest.com>
2019-08-02 10:34:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-    require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-      var map = new Map({
-        basemap: "streets"
+require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+        var map = new Map({
+          basemap: "streets"
+        });
+        var view = new MapView({
+          container: "viewDiv", // Reference to the scene div created in step 5
+          map: map, // Reference to the map object created before the scene
+          zoom: 4, // Sets zoom level based on level of detail (LOD)
+          center: [15, 65] // Sets center point of view using longitude,latitude
+        });
       });
-      var view = new MapView({
-        container: "viewDiv",
-        map: map,
-        zoom: 4,
-        center: [15, 65] // longitude, latitude
-      });
-    });
-
-      
