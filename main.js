a33e0e240c06654261e6b2ea1876256c9157602e
Pawling <bpawling@eaest.com>
2019-08-02 16:28:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer"], function(Map, MapView,TileLayer) {
         
-var transportationLayer = new TileLayer({url: "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer",id: "streets", opacity: 0.7});
+var transportationLayer = new TileLayer({url: "https://services3.arcgis.com/f8FBNX4bfDyc804R/arcgis/rest/services/Locations/FeatureServer",opacity: 0.7});
 
-var housingLayer = new TileLayer({url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/New_York_Housing_Density/MapServer",id: "ny-housing"});
+
 
 var map = new Map({basemap: "oceans", layers: [housingLayer]});
         var view = new MapView({
