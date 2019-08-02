fa35486875bb31e3d4be25d1a2a5e96df0da7dc7
Pawling <bpawling@eaest.com>
2019-08-02 10:33:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,13 @@
-require([
-          "esri/Map",
-          "esri/views/MapView",
-	  "esri/Graphic", 
-	  "dojo/domReady!"
-      ],
-      function(
-          Map, MapView,Graphic) {
-          //create map 
-          var map = new Map({
-              basemap: "topo"
-          });
-
-          var lon = -95.7129;
-          lat = 37.0902;
-
-	  
-
-          //view the map 
-          var view = new MapView({
-              container: "viewDiv",
-              map: map,
-              zoom: 4,
-              center: [lon, lat],
-	            
-          });
-var point = {
-        type: "point",
-        longitude: -118.29507,
-        latitude: 34.13501
-      };
-
-      var simpleMarkerSymbol = {
-        type: "simple-marker",
-        color: [226, 119, 40],  // orange
-        outline: {
-          color: [255, 255, 255], // white
-          width: 1
-        }
-      };
-
-      var pointGraphic = new Graphic({
-        geometry: point,
-        symbol: simpleMarkerSymbol
+    require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+      var map = new Map({
+        basemap: "streets"
       });
-
-      view.graphics.add(pointGraphic);
+      var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        zoom: 4,
+        center: [15, 65] // longitude, latitude
+      });
     });
-
-      });
 
       
