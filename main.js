0bce7b7b1dbc2f31f484ae1ba0d2b4bf7efede20
Pawling <bpawling@eaest.com>
2019-08-02 11:31:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "streets"
-        });
+require(["esri/Map", "esri/views/MapView", "esri/layers/TileLayer"], function(Map, MapView,TileLayer) {
+        
+var transportationLayer = new TileLayer({url: "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer"});
+
+ var housingLayer = new TileLayer({url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/New_York_Housing_Density/MapServer"});
+
+var map = new Map({basemap: "oceans",layers: [housingLayer]});
         var view = new MapView({
           container: "viewDiv", // Reference to the scene div created in step 5
           map: map, // Reference to the map object created before the scene
