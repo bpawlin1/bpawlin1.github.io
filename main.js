9f76f7ee789fc48f3523fb18f998e1a1e1b240f7
bpawlin1 <47511183+bpawlin1@users.noreply.github.com>
2019-08-13 14:06:36 -0400

    1

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-require(["esri/Map","esri/views/MapView","esri/layers/FeatureLayer"], function(Map, MapView, FeatureLayer) {
+    require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer"], function(Map, MapView,FeatureLayer) {
+        var map = new Map({
+          basemap: "terrain"
+        });
 
-      var map = new Map({
-        basemap: "topo-vector"
+        var view = new MapView({
+          container: "viewDiv",
+          map: map,
+          zoom: 13,
+          center: [-118.80543,34.02700] // longitude, latitude
+        });
+// Trailheads feature layer (points)
+      var trailheadsLayer = new FeatureLayer({
+        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
       });
-
-      var view = new MapView({
-        container: "viewDiv",
-        map: map,
-        center: [-118.80543,34.02700],
-        zoom: 13
-      });
-      var trailheadsLayer = new FeatureLayer({url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"});
 
       map.add(trailheadsLayer);
-    });
+      });
