9d6e90966c819cb7fb7c89fde93ea3329b168f97
bpawlin1 <47511183+bpawlin1@users.noreply.github.com>
2019-08-13 11:41:28 -0400

    2

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function(Map, MapView,FeatureLayer) {
-        
-var trailheadsLayer = new FeatureLayer({url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0",opacity: 0.7});
+require([
+      "esri/Map",
+      "esri/views/MapView","esri/layers/FeatureLayer"
+    ], function(Map, MapView, FeatureLayer) {
 
-
-
-var map = new Map({basemap: "oceans", layers: [trailheadsLayer]});
-        var view = new MapView({
-          container: "viewDiv", // Reference to the scene div created in step 5
-          map: map, // Reference to the map object created before the scene
-          zoom: 10, // Sets zoom level based on level of detail (LOD)
-           center: [-73.87, 40.70]  // Sets center point of view using longitude,latitude
-        });
+      var map = new Map({
+        basemap: "topo-vector"
       });
+
+      var view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [-118.80543,34.02700],
+        zoom: 13
+      });
+var trailheadsLayer = new FeatureLayer({
+        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+      });
+
+      map.add(trailheadsLayer);
+    });
