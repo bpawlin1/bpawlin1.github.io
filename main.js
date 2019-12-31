c5b8e3294c9b9f4abf3a7f32ba6caa8ef59a3c9f
bpawlin1 <47511183+bpawlin1@users.noreply.github.com>
2019-12-30 21:37:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-// map view
-
-    require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer"], function(Map, MapView,FeatureLayer) {
-        var map = new Map({
-          basemap: "terrain"
-        });
-
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 13,
-          center: [-118.80543,34.02700] // longitude, latitude
-        });
-// Trailheads feature layer (points)
-      var trailheadsLayer = new FeatureLayer({
-        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
-      });
-
-      map.add(trailheadsLayer);
-      });
-
-
-
 // Pasword zone
     function passWord() 
   {
@@ -42,3 +19,31 @@
     history.go(-1);
     return " ";
   } 
+
+
+
+
+// map view
+    
+    require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer"], function(Map, MapView,FeatureLayer) {
+        var map = new Map({
+          basemap: "terrain"
+        });
+
+        var view = new MapView({
+          container: "viewDiv",
+          map: map,
+          zoom: 13,
+          center: [-118.80543,34.02700] // longitude, latitude
+        });
+// Trailheads feature layer (points)
+      var trailheadsLayer = new FeatureLayer({
+        url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+      });
+
+      map.add(trailheadsLayer);
+      });
+  
+
+
+
